331f17fa7c5fdd9237ae2e2185b62648b691384d
Willie Colatino <wcolatino@hotmail.com>
2026-04-20 19:56:55 -0300

    Correção dos imports de Proprietario.java

--- src/main/java/com/westside/controledecarros/entity/Proprietario.java
+++ src/main/java/com/westside/controledecarros/entity/Proprietario.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "proprietarios")
 @Getter
